e41633a852ea4baab0a578282478190d09f3ea74
cse-f <verify.moneymind@gmail.com>
2024-11-19 23:36:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 ADA
-
-
-
+ARTICULATION POINTS----------->       curl -k https://cse-f.github.io/ads/ada/articulation.js
+DIJKSTRAS--------------------->       curl -k https://cse-f.github.io/ads/ada/dijkstra.js
+job sequencing---------------->       curl -k https://cse-f.github.io/ads/ada/job_scheduling.js
+knapsack---------------------->       curl -k https://cse-f.github.io/ads/ada/knapsack.js
+merge------------------------->       curl -k https://cse-f.github.io/ads/ada/merge.js
+obst-------------------------->       curl -k https://cse-f.github.io/ads/ada/obst.js
+prims------------------------->       curl -k https://cse-f.github.io/ads/ada/prims.js
+quick sort-------------------->       curl -k https://cse-f.github.io/ads/ada/quick.js
+sum of subsets---------------->       curl -k https://cse-f.github.io/ads/ada/sumOfSubsets.js
 
 
 DS
